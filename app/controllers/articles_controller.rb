b9c06d5c055521be3eb3931fc11ee70f70248eb8
Matsumaru-Kachimaru <64599456+Matsumaru-Kachimaru@users.noreply.github.com>
2020-05-09 11:51:50 +0900

    delete comments

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,28 +1,20 @@
 class sController < ApplicationController
   before_action :set_, only: [:show, :edit, :update, :destroy]
 
-  # GET /s
-  # GET /s.json
   def index
     @s = .all
   end
 
-  # GET /s/1
-  # GET /s/1.json
   def show
   end
 
-  # GET /s/new
   def new
     @ = .new
   end
 
-  # GET /s/1/edit
   def edit
   end
 
-  # POST /s
-  # POST /s.json
   def create
     @ = .new(_params)
 
@@ -37,8 +29,6 @@ class sController < ApplicationController
     end
   end
 
-  # PATCH/PUT /s/1
-  # PATCH/PUT /s/1.json
   def update
     respond_to do |format|
       if @.update(_params)
@@ -50,9 +40,6 @@ class sController < ApplicationController
       end
     end
   end
-
-  # DELETE /s/1
-  # DELETE /s/1.json
   def destroy
     @.destroy
     respond_to do |format|
@@ -62,12 +49,10 @@ class sController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_
       @ = .find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def _params
       params.require(:).permit(:title, :author, :text)
     end
